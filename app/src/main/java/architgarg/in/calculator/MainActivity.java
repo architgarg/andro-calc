6724c9b151f3cfbf5f39ed8971ecbcc9446bb1af
Archit Garg <architgarg1997@gmail.com>
2016-08-27 23:45:37 +0530

    Decimal Bug Removed

--- app/src/main/java/architgarg/in/calculator/MainActivity.java
+++ app/src/main/java/architgarg/in/calculator/MainActivity.java
@@ -8,25 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private Button button1;
-//    private Button button2;
-//    private Button button3;
-//    private Button button4;
-//    private Button button5;
-//    private Button button6;
-//    private Button button7;
-//    private Button button8;
-//    private Button button9;
-//    private Button button0;
-//    private Button buttonDot;
-//    private Button buttonEqual;
-//    private Button clearButton;
-//    private Button divideButton;
-//    private Button multiplyButton;
-//    private Button plusButton;
-//    private Button minusButton;
-
-
     public TextView text1 = null;
     public TextView text2 = null;
     private float a = 0;
@@ -42,24 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
         text1 = (TextView) findViewById(R.id.textView1);
         text2 = (TextView) findViewById(R.id.textView2);
-
-//        button0 = (Button)findViewById(R.id.button0);
-//        button1 = (Button)findViewById(R.id.button1);
-//        button2 = (Button)findViewById(R.id.button2);
-//        button3 = (Button)findViewById(R.id.button3);
-//        button4 = (Button)findViewById(R.id.button4);
-//        button5 = (Button)findViewById(R.id.button5);
-//        button6 = (Button)findViewById(R.id.button6);
-//        button7 = (Button)findViewById(R.id.button7);
-//        button8 = (Button)findViewById(R.id.button8);
-//        button9 = (Button)findViewById(R.id.button9);
-//        buttonDot = (Button)findViewById(R.id.buttonDot);
-//        clearButton = (Button)findViewById(R.id.clear_button);
-//        buttonEqual = (Button)findViewById(R.id.buttonEqual);
-//        divideButton = (Button)findViewById(R.id.divide_button);
-//        multiplyButton = (Button)findViewById(R.id.multiply_button);
-//        plusButton = (Button)findViewById(R.id.plus_button);
-//        minusButton = (Button)findViewById(R.id.minus_button);
     }
 
     public void button0Clicked(View view) {
@@ -111,10 +74,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonMultiplyClicked(View view) {
-        char c = text1.getText().toString().charAt(text1.getText().toString().length() - 1);
-        if (c == '.') {
-            text1.append("0");
-        }
         if (flag == 0 && !text1.getText().toString().equals(" ")) {
             text1.append("X");
             flag = 1;
@@ -123,10 +82,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonDivideClicked(View view) {
-        char c = text1.getText().toString().charAt(text1.getText().toString().length() - 1);
-        if (c == '.') {
-            text1.append("0");
-        }
         if (flag == 0 && !text1.getText().toString().equals(" ")) {
             text1.append("/");
             flag = 1;
@@ -135,10 +90,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonPlusClicked(View view) {
-        char c = text1.getText().toString().charAt(text1.getText().toString().length() - 1);
-        if (c == '.') {
-            text1.append("0");
-        }
         if (flag == 0 && !text1.getText().toString().equals(" ")) {
             text1.append("+");
             flag = 1;
@@ -147,10 +98,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonMinusClicked(View view) {
-        char c = text1.getText().toString().charAt(text1.getText().toString().length() - 1);
-        if (c == '.') {
-            text1.append("0");
-        }
         if (flag == 0 && !text1.getText().toString().equals(" ")) {
             text1.append("-");
             flag = 1;
